6631f229b32462b4834e3bf0722a62aa406704c5
urandias <urandias@gmail.com>
2024-12-10 16:37:13 +0000

    Initial commit: started project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Name from "./components/Name"
+import Nav from "./components/Nav"
+
+function App() {
+
+  return (
+    <>
+    <body>
+    <Nav/>
+    <Name/>
+    </body>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Name.jsx
@@ -0,0 +1,8 @@
+
+const Name = () => {
+    return (
+        <header className="name-text">Uran Dias</header>
+    )
+}
+
+export default Name
