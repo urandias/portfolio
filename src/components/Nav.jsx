7bf35a4b7bb5646028c42ac59a43cbbfeeca82fc
urandias <urandias@gmail.com>
2024-12-11 12:12:15 +0000

    added nav icon, cleaned up code a little

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import styled from 'styled-components';
 
 const Nav = () => {
   return (
-    <StyledWrapper>
+    <nav>
       <label className="hamburger">
         <input type="checkbox" />
         <svg viewBox="0 0 32 32">
@@ -11,48 +10,8 @@ const Nav = () => {
           <path className="line" d="M7 16 27 16" />
         </svg>
       </label>
-    </StyledWrapper>
+    </nav>
   );
 }
-
-const StyledWrapper = styled.div`
-  .hamburger {
-    cursor: pointer;
-  }
-
-  .hamburger input {
-    display: none;
-  }
-
-  .hamburger svg {
-    /* The size of the SVG defines the overall size */
-    height: 3em;
-    /* Define the transition for transforming the SVG */
-    transition: transform 600ms cubic-bezier(0.4, 0, 0.2, 1);
-  }
-
-  .line {
-    fill: none;
-    stroke: white;
-    stroke-linecap: round;
-    stroke-linejoin: round;
-    stroke-width: 3;
-    /* Define the transition for transforming the Stroke */
-    transition: stroke-dasharray 600ms cubic-bezier(0.4, 0, 0.2, 1),
-                stroke-dashoffset 600ms cubic-bezier(0.4, 0, 0.2, 1);
-  }
-
-  .line-top-bottom {
-    stroke-dasharray: 12 63;
-  }
-
-  .hamburger input:checked + svg {
-    transform: rotate(-45deg);
-  }
-
-  .hamburger input:checked + svg .line-top-bottom {
-    stroke-dasharray: 20 300;
-    stroke-dashoffset: -32.42;
-  }`;
 
 export default Nav;
